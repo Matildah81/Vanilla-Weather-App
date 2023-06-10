41d77c7d57472b475a16a456801fe78114b8581b
Matildah81 <matildahnkambule@gmail.com>
2023-06-10 07:56:51 +0200

    updated style.css,app.js and html

--- src/app.js
+++ src/app.js
@@ -1,91 +1,52 @@
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-let hours = currentTime.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = currentTime.getMinutes();
-if (minutes < 10) {
-  hours = `0${minutes}`;
-}
-let dayIndex = currentTime.getDay();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+      hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+      minutes = `0${minutes}`;
+  }
+
+  let days = [
+      "Sunday", 
+  "Monday", 
+  "Tuesday", 
   "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saterday"
-];
+  "Thursday", 
+  "Friday", 
+  "Saterday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 
-function searchLocation(position) {
-  let apikey = "837f38326f069f2010934d6ca6dd33be";
-  let apiurl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.lonitude}&appid=${apikey}&unit=metric`;
-  console.log(apiurl);
-  axios.get(apiurl).then(disaplayWeatherCondition);
 }
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-function disaplayWeatherCondition(response) {
-  console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-let iconElement = document.querySelector("#icon");
-iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-}
-
-function searchCity(city) {
-  let apikey = "837f38326f069f2010934d6ca6dd33be";
-  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-  axios.get(apiurl).then(disaplayWeatherCondition);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-
-  searchCity(city);
-}
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = 66;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate("response.data.dt. * 1000");
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+  
+  
 
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 12;
-}
+let apiKey = "27198170573855eebccdea81a3d80fe8";
+let city = "Johannesburg"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiustLink = document.querySelector("#celsius-link");
-celsiustLink.addEventListener("click", convertToCelsius);
-let searchForm = document.querySelector("#search-form");
-
-searchForm.addEventListener("submit", handleSubmit);
-
-dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-
-
-searchCity("New York");
+axios.get(apiUrl).then(displayTemperature)
